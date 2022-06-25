dd4761e69454a8c1c9fbdd6209bb269efa77e459
Shao Tan <t.shaoming@gmail.com>
2022-06-25 16:47:36 +0800

    install express eslint typescript

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
-const firebaseConfig = {
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
